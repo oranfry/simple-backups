0c4c9ea7413101e6c0c7a25f17e1b7861ba240fb
Oran Fry <oran@emerg.co>
2020-03-12 14:37:58 +1300

    verbose option

--- backup-server.sh
+++ backup-server.sh
@@ -6,8 +6,9 @@ parent_dir=backup
 backup_owner=root
 tar_opts=
 invalid=
+verbose=
 
-while getopts ":o:p:zs" opt; do
+while getopts ":o:p:zsv" opt; do
   case ${opt} in
     o )
       backup_owner=$OPTARG
@@ -22,6 +23,9 @@ while getopts ":o:p:zs" opt; do
     s )
       save_space=1
       tar_opts="--remove-files"
+      ;;
+    v )
+      verbose=1
       ;;
     \? )
       echo "Invalid option: $OPTARG" 1>&2
@@ -48,12 +52,16 @@ fi
 
 CLONE_HOME="/root/.clone/${host}"
 TAR_HOME="$(eval echo ~$backup_owner)/${parent_dir}/${host}"
+TAR_FILE="${TAR_HOME}/backup.tar${gzip_ext}"
+
+test -n "$verbose" && echo -e "CLONE_HOME: $CLONE_HOME\nTAR_HOME: $TAR_HOME\nTAR_FILE: $TAR_FILE\n"
 
 if [ -e "${TAR_HOME}/processing" ]; then
 	echo "Cowardly refusing to run two backup processes at the same time" 1>&2
 	exit 1
 fi
 
+test -n "$verbose" && echo "Locking..."
 echo '1' > "${TAR_HOME}/processing"
 
 mkdir -p "${CLONE_HOME}"
@@ -62,12 +70,15 @@ mkdir -p "${TAR_HOME}"
 
 chown $backup_owner:$backup_owner "${TAR_HOME}"
 
-if [ -n "${save_space}" -a -s "${TAR_HOME}/backup.tar${gzip_ext}" ]; then
-  rm -rf "${CLONE_HOME}" && mkdir "${CLONE_HOME}" && cd "${CLONE_HOME}" && /bin/tar -xf${gzip_flag} "${TAR_HOME}/backup.tar${gzip_ext}"
+if [ -n "${save_space}" -a -s "${TAR_FILE}" ]; then
+  test -n "$verbose" && echo "Extracting existing backup..."
+  rm -rf "${CLONE_HOME}" && mkdir "${CLONE_HOME}" && cd "${CLONE_HOME}" && /bin/tar -x${gzip_flag}f "${TAR_FILE}"
 fi
 
 includesrel=$(cat "${TAR_HOME}/files.list" | while read f; do while [ "$f" != "/" ]; do echo "$f"; f="$(dirname "$f")"; done; done | sort | uniq | while read f; do echo " '--include=$(echo $f | sed 's,^/,,')'"; done)
 cmd="/usr/bin/rsync -a --delete $includesrel '--exclude=*' '${CLONE_HOME}/' '${CLONE_HOME}.new'"
+
+test -n "$verbose" && echo -e "Pruning cache based on file patterns...\n$cmd\n"
 eval $cmd
 
 rm -rf "${CLONE_HOME}"
@@ -75,14 +86,21 @@ mv "${CLONE_HOME}.new" "${CLONE_HOME}"
 
 includes=$(cat "${TAR_HOME}/files.list" | while read f; do while [ "$f" != "/" ]; do echo "$f"; f="$(dirname "$f")"; done; done | sort | uniq | while read f; do echo " '--include=$f'"; done)
 cmd="/usr/bin/rsync --rsync-path='sudo rsync' -az --delete $includes '--exclude=*' ${host}:/ '${CLONE_HOME}'"
+
+test -n "$verbose" && echo -e "Rsync'ing with server...\n$cmd\n"
 eval $cmd
 
-rm -f "${TAR_HOME}/backup.tar${gzip_ext}" # frees up space for the new archive
+test -n "$verbose" && echo "Removing existing backup..."
+rm -f "${TAR_FILE}" # frees up space for the new archive
 
-(cd "${CLONE_HOME}" && /bin/tar ${tar_opts} -cf${gzip_flag} "/tmp/${host}.tar${gzip_ext}" * && chown $backup_owner:$backup_owner "/tmp/${host}.tar${gzip_ext}" && mv "/tmp/${host}.tar${gzip_ext}" "${TAR_HOME}/backup.tar${gzip_ext}")
+TAR_FILE_TMP="/tmp/${host}.tar${gzip_ext}"
+test -n "$verbose" && echo "Creating new backup..."
+(cd "${CLONE_HOME}" && /bin/tar $tar_opts -c${gzip_flag}f "${TAR_FILE_TMP}" * && chown $backup_owner:$backup_owner "${TAR_FILE_TMP}" && mv "${TAR_FILE_TMP}" "${TAR_FILE}")
 
-if [ -n "${save_space}" -a -s "${TAR_HOME}/backup.tar${gzip_ext}" ]; then
+if [ -n "${save_space}" -a -s "${TAR_FILE}" ]; then
+  test -n "$verbose" && echo "Clearing cache..."
   rm -rf "${CLONE_HOME}"
 fi
 
+test -n "$verbose" && echo "Unlocking..."
 rm "${TAR_HOME}/processing"
